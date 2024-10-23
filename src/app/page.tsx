8c645c56a982004220f2248bbb43b88fb292c016
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-10-23 22:26:52 +0100

    getting started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <>
       <h1>Take a Seat</h1>
+      <p>Don`&apos t feel left out anymore </p>
       <Image
         src="../images/main-foto.avif"
-        alt="restaurant party"
+        alt="restaurant party for take a seat"
         width={500}
         height={500}
       />
