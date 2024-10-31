85edc22be5ee4104070339e18e5d3e48f245d716
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-10-31 14:28:46 +0000

    create databease on firestore and seed it with fake data

---
+++ src/app/about/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function About() {
+  return React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "About"), /*#__PURE__*/React.createElement("p", null, "This is the about page"));
+}
